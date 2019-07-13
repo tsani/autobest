8d44550237020a4280743ffda8dfcd9457fe3b02
Jacob Errington <git@mail.jerrington.me>
2019-07-13 18:36:52 -0400

    new irssi_loader features and new experiments

--- irssi_loader.py
+++ irssi_loader.py
@@ -1,5 +1,6 @@
 from collections import namedtuple, defaultdict
 import re
+import numpy as np
 
 MESSAGE_LINE_REGEX = re.compile('"(\d+)"<.(\w+)> (.*)\n')
 
@@ -34,9 +35,70 @@ def histogram(entries):
         h[e.time].add(e)
     return h
 
+class id_mapper:
+    """Wrapper around a defaultdict for assigning unique identifiers
+    to hashable objects.
+    """
+    def __init__(self):
+        self.reset()
+        
+    def __getitem__(self, k):
+        if self._back[k] == -1:
+            self._back[k] = self._next_id
+            self._next_id += 1
+        return self._back[k]
+
+    def reset(self):
+        self._next_id = 0
+        self._back = defaultdict(lambda: -1)
+
+    def  __len__(self):
+        return len(self._back)
+
+def histogram_to_array(h, t_min, t_max):
+    users = id_mapper()
+
+    a = []
+
+    for i in range(t_min, t_max + 1):
+        entries = h[i]
+        ids = np.array([users[e.user] for e in entries])
+        a.append(ids)
+
+    return np.array(a)
+
+def user_ids(entries):
+    """Computes a map from usernames to unique numbers."""
+    users = id_mapper()
+    for e in entries:
+        users[e.user]
+    return users
+
+def histogram_to_matrix(entries, users=None):
+    if users is None:
+        users = user_ids(entries)
+    n = len(users)
+    # note: n is the least upper bound of users
+    t_min, t_max = entries[0].time, entries[-1].time
+    row_count = t_max + 1 - t_min
+    m = np.ndarray( (row_count, n), dtype=np.float64 )
+    max_msgs = 0.0
+    for e in entries:
+        m[e.time - t_min, users[e.user]] += 1
+        p = m[e.time - t_min, users[e.user]]
+        if p > max_msgs:
+            max_msgs = p
+    m /= max_msgs
+    print("max_msgs", max_msgs)
+    return m
+
 def main(args):
     entries = load(args[1])
+    users = user_ids(entries)
+    
     h = histogram(entries)
+    a = histogram_to_array(h, entries[0].time, entries[-1].time)
+    m = histogram_to_matrix(entries, users)
     print("loaded", len(h), "entries")
 
 if __name__ == '__main__':
